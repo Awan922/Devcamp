fa897576fa85b6778941495de43c1d16f4f13092
Muhammad Awan <msa922@yahoo.com>
2017-06-15 16:08:41 -0600

    added controller concerns for dvise prams

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,4 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-
-  before_filter :configuer_permitted_parameters, if: :devise_controller?
-
- def configuer_permitted_parameters
-	 devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
-	 devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+   include DeviseWhitelist
   end
-
-end
