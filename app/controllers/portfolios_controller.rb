7ddb3619617cb6840538d2c5897d14b5af8efc90
Muhammad Awan <msa922@yahoo.com>
2017-06-21 15:12:40 -0600

    Implemented authorization engine for portfolio items and blogs

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,5 +1,7 @@
 class PortfoliosController < ApplicationController
+  before_action :set_portfolio_item, only: [:edit, :show, :update, :destroy]
   layout 'portfolio'
+  access all: [:show, :index, :angular], user: {except: [:destroy, :new, :create, :update, :edit]}, site_admin: :all
   
 	def index
 		@portfolio_items = Portfolio.all
@@ -18,41 +20,33 @@ def create
     @portfolio_item = Portfolio.new(portfolio_params)
 
     respond_to do |format|
-      if @blog.save
+      if @portfolio_item.save
         format.html { redirect_to portfolios path, notice: 'Your portfolio item is now live.' }
-        format.json { render :show, status: :created, location: @blog }
       else
         format.html { render :new }
-        format.json { render json: @blog.errors, status: :unprocessable_entity }
       end
     end
   end
 def edit 
-	@portfolio_item = Portfolio.find(params[:id])
-
 end
 
 	def update
-    @portfolio_item = Portfolio.find(params[:id])
-
     respond_to do |format|
       if @portfolio_item.update(portfolio_params)
         format.html { redirect_to portfolios_path, notice: 'Blog was successfully updated.' }
-        format.json { render :show, status: :ok, location: @blog }
       else
         format.html { render :edit }
-       
-      end
+       end
     end
   end
+
 def show
-  @portfolio_item = Portfolio.find(params[:id])
 end
 
   def destroy
-   @portfolio_item = Portfolio.find(params[:id])
 
    @portfolio_item.destroy
+
    respond_to do |format|
     format.html {redirect_to portfolios_url, notice: 'record was removed'}
   end
@@ -67,6 +61,9 @@ end
                                       technologies_attributes: [:name]
                                       )
 
+   end
+   def set_portfolio_item
+     @portfolio_item = Portfolio.find(params[:id])
    end
  
   end
